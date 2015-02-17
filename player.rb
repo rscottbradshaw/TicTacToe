8c80ac2b203bd1a26cc049e0c2058d3fe819c690
R. Scott Bradshaw <scottbradshaw@nc.rr.com>
2015-02-17 11:12:07 -0500

    player turn will always mark postive squares now

--- player.rb
+++ player.rb
@@ -24,6 +24,7 @@ class Player
       take_turn
     else
       mark_square(location)
+      return @board.player_moves << (location + 9) if location < 0
       @board.player_moves << location
     end
   end
